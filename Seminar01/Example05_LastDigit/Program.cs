845973df4c1e0aa46e0223cdede2cfc502970b59
OksanaLebedeva <122013217+OksanaLebedeva@users.noreply.github.com>
2023-01-15 21:29:24 +0300

    Last digit

---
+++ Seminar01/Example05_LastDigit/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.Write("Please enter 3-digit number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num<1000)
+{
+  if (num>99)
+  {
+    Console.Write("The last digit is: ");
+    Console.WriteLine(num % 10);
+  }
+  else
+  {
+    Console.WriteLine("You entered incorrect number, please try again");
+  }
+}
+else
+{
+  Console.WriteLine("You entered incorrect number, please try again");
+}
