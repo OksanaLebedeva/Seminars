eab730961b7e9a413d2b7705c6527cb1ebe953a0
OksanaLebedeva <122013217+OksanaLebedeva@users.noreply.github.com>
2023-01-15 21:16:22 +0300

    WeekDay check

---
+++ Seminar01/Example03_WeekDay/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Please enter the number from 1 to 7: ");
+int weekday = Convert.ToInt32(Console.ReadLine());
+if (weekday==1) {Console.WriteLine("It's Monday");}  
+if (weekday==2) {Console.WriteLine("It's Tuesday");}
+if (weekday==3) {Console.WriteLine("It's Wednesday");}
+if (weekday==4) {Console.WriteLine("It's Thursday");}
+if (weekday==5) {Console.WriteLine("It's Friday");}
+if (weekday==6) {Console.WriteLine("It's Saturday");}
+if (weekday==7) {Console.WriteLine("It's Sunday");}
+if (weekday<1) {Console.WriteLine("You entered wrong number, bye");}
+if (weekday>7) {Console.WriteLine("You entered wrong number, bye");}
