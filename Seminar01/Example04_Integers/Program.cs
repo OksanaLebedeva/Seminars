dda311d068a9241b6eb444591914f374cfabba1b
OksanaLebedeva <122013217+OksanaLebedeva@users.noreply.github.com>
2023-01-15 21:23:08 +0300

    Integers

---
+++ Seminar01/Example04_Integers/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("Please enter the number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num<0)
+{
+  num = -num;
+}
+int count = -num;
+while(count<=num)
+{
+  Console.Write(count);
+  Console.Write(" ");
+  count++;
+}
+Console.WriteLine("The end");
