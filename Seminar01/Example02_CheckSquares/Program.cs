e95dd642ebe08d1a204d66778173eba254b7fcda
OksanaLebedeva <122013217+OksanaLebedeva@users.noreply.github.com>
2023-01-15 20:57:25 +0300

    1st task

---
+++ Seminar01/Example02_CheckSquares/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Please enter first number: ");
+int square = Convert.ToInt32(Console.ReadLine());
+Console.Write("Please enter second number: ");
+int root = Convert.ToInt32(Console.ReadLine());
+if (square==root*root)
+{
+  Console.WriteLine("First number is a square of the second number!");
+}
+else
+{
+  Console.WriteLine("Unfortunately, first number is NOT a square of the second number");
+}
