4a283486caf6cf217b5adc8d3483ffcd9c569ec4
OksanaLebedeva <122013217+OksanaLebedeva@users.noreply.github.com>
2023-01-15 20:51:09 +0300

    Squares

---
+++ Seminar01/Example01_Squares/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Please enter the number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number<0)
+{
+  Console.Write("Square is: ");
+  Console.WriteLine( number*number );
+}
+else
+{
+  Console.Write("The remainder of the division by 2 is: ");
+  Console.WriteLine(number % 2);
+}
