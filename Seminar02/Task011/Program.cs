c9da47ba8265a88558709ef581173a5705be7193
OksanaLebedeva <122013217+OksanaLebedeva@users.noreply.github.com>
2023-01-18 14:20:18 +0300

    First task

---
+++ Seminar02/Task011/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write("Random 3-digit number: ");
+int num = new Random().Next (100,1000);
+Console.WriteLine(num);
+int dig1 = num / 100;
+int dig2 = num % 10;
+Console.Write("Result is: ");
+Console.WriteLine($"{dig1}{dig2}");
