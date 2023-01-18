e9e5e0ce86fa7b934a0a1ff7f50b4a14e32af21d
OksanaLebedeva <122013217+OksanaLebedeva@users.noreply.github.com>
2023-01-18 14:25:45 +0300

    Task14

---
+++ Seminar02/Task012/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Please enter first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Please enter second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int ost = num1 % num2;
+if (ost == 0)
+{
+  Console.WriteLine("YESSSS!");
+}
+else
+{
+  Console.WriteLine(ost);
+}
