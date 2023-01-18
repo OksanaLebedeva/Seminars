d0d1ae5d3e150e518f8c07737709f18b5e907026
OksanaLebedeva <122013217+OksanaLebedeva@users.noreply.github.com>
2023-01-18 14:37:33 +0300

    Task16

---
+++ Seminar02/Task016/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Please enter first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Please enter second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if ((num1*num1==num2) || (num2*num2==num1))
+{
+  Console.WriteLine("It is a win!");
+}
+else Console.WriteLine("It is a loss");
