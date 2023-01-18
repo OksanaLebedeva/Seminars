abd88a977c361c11f98684df430fa0c637ae2af5
OksanaLebedeva <122013217+OksanaLebedeva@users.noreply.github.com>
2023-01-18 14:32:30 +0300

    Task16

---
+++ Seminar02/Task014/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Please enter your number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if ((num % 7 == 0) && (num % 23 == 0))
+{
+  Console.WriteLine("Congratulations!");
+}
+else
+{
+  Console.WriteLine("My regrets...");
+}
