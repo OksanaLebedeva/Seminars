da80818d1f11671552e6f061784ee91fd58377a9
OksanaLebedeva <122013217+OksanaLebedeva@users.noreply.github.com>
2023-01-24 10:32:04 +0300

    Seminar 06

---
+++ Seminar06/Program.cs
@@ -0,0 +1,12 @@
+﻿// int[] num1 = {1,2,3,4,5};
+// int[,] num2 = {{1,2,3},{4,5,6}};
+// int[,] num3 = new int[2,3];
+// for (int i=0; i<num2.GetLength(0);i++)
+// {
+//   for (int j=0; j<num2.GetLength(1); j++)
+//   {
+//     Console.Write($"{num2[i,j]} ");
+//   }
+//   Console.WriteLine();
+// }
+
